99b102fec36a5a194bdf6837dd0664aa0a096369
Ilxomjon <abdullayevilxomjon1@gmail.com>
2021-06-17 22:56:01 +0500

    ui fixes

--- src/views/sellers/Cart.vue
+++ src/views/sellers/Cart.vue
@@ -1,32 +1,28 @@
 <template>
-  <div class="main main-raised">
-    <div class="container">
-      <div class="card m-4">
-        <div class="card-body">
-          <h3 class="card-title mb-2">Sizning Buyurtmalaringiz</h3>
-          <div class="table-responsive">
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <th>Mahsulot nomi</th>
-                  <th>Soni</th>
-                  <th>Narxi</th>
-                  <th>Holati</th>
-                  <th>Izoh</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="item in getCart" :key="item.id">
-                  <td>{{ item.name }}</td>
-                  <td>{{ item.amount }}</td>
-                  <td>{{ item.name }}</td>
-                  <td>{{ item.status }}</td>
-                  <td>{{ item.description }}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+  <div class="card mt-5 mr-1 ml-1">
+    <div class="card-body">
+      <h3 class="card-title mb-2">Sizning Buyurtmalaringiz</h3>
+      <div class="table-responsive">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>Mahsulot nomi</th>
+              <th>Soni</th>
+              <th>Narxi</th>
+              <th>Holati</th>
+              <th>Izoh</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in getCart" :key="item.id">
+              <td>{{ item.name }}</td>
+              <td>{{ item.amount }}</td>
+              <td>{{ item.name }}</td>
+              <td>{{ item.status }}</td>
+              <td>{{ item.description }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
